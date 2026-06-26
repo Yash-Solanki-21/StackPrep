1ddbcd297d11419ec4ab363edd6730c854ff33d8
Yash Solanki <yashsolankiji21@gmail.com>
2026-06-26 18:53:48 +0530

    add a checking route

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,12 @@ import "dotenv/config";
 import { app } from "./app.js";
 import { connectToDB } from "./src/db/database.js";
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "StackPrep Backend is running 🚀",
+  });
+});
 
 connectToDB();
 
