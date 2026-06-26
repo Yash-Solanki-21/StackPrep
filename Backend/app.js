298a16f16f897399a6be8b54dc16ac2a8a4dd551
Yash Solanki <yashsolankiji21@gmail.com>
2026-06-26 21:21:10 +0530

    cors updated

--- Backend/app.js
+++ Backend/app.js
@@ -3,10 +3,22 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://stack-prep-phi.vercel.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }));
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
